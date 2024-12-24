dca96c2fdbac7afc3c2696be850e3ed4972ee019
Aniruddha Bhajan <abhajan@truefit.com>
2024-12-24 19:41:32 +0530

    Added necessary constants

--- Sources/truefit-ios-package-restapi/Constants.swift
+++ Sources/truefit-ios-package-restapi/Constants.swift
@@ -6,3 +6,12 @@
 //
 
 
+public enum TFEnvironment {
+  case staging
+  case production
+}
+
+internal let TFProdDomain = "consumer.truefitcorp.com"
+internal let TFStagingDomain = "ds-prod-ga-2-tf-ds-c503f2f8.truefitcorp.com"
+internal let tfTokenKey = "tfToken"
+internal let tfUserTokenHeader = "X-TF-User-Token"
