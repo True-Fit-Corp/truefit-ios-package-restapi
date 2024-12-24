961182f02227d2688df39486a1787f9726312ed8
Aniruddha Bhajan <abhajan@truefit.com>
2024-12-24 19:09:26 +0530

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "truefit-ios-package-restapi",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,10 +16,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "truefit-ios-package-restapi"),
+            name: "truefit-ios-package-restapi",
+            path: "Sources/truefit-ios-package-restapi"),
         .testTarget(
             name: "truefit-ios-package-restapiTests",
-            dependencies: ["truefit-ios-package-restapi"]
+            dependencies: ["truefit-ios-package-restapi"],
+            path: "Tests/truefit-ios-package-restapiTests"
         ),
     ]
 )
---
+++ Sources/truefit-ios-package-restapi/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  truefit-ios-package-restapi
+//
+//  Created by Aniruddha Bhajan on 24/12/24.
+//
+
+
